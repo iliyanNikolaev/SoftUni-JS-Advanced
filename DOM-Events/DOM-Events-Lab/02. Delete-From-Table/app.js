d847d0572203ac64a7d25a56361b424ebc82dc2a
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-03-18 22:29:05 +0200

    init

--- DOM-Events/DOM-Events-Lab/02. Delete-From-Table/app.js
+++ DOM-Events/DOM-Events-Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,26 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let inputElement = document.querySelector('input[name="email"]');
+
+    let targetEmail = inputElement.value;
+
+    let emailCellsElements = document.querySelectorAll('tr td:nth-child(2)');
+
+    let resultDivElement = document.getElementById('result');
+
+    let isDeleted = false;
+
+    for (const email of emailCellsElements) {
+
+        if (email.textContent === targetEmail) {
+            email.parentNode.remove();
+
+            resultDivElement.textContent = 'Deleted.'
+
+            isDeleted = true;
+        }    
+    }
+
+    if(!isDeleted){
+        resultDivElement.textContent = 'Not found.'
+    }
 }
--- DOM-Introduction/DOM-Introduction-Exr/08. Generate Report/report.js
+++ DOM-Introduction/DOM-Introduction-Exr/08. Generate Report/report.js
@@ -1,9 +1,34 @@
 function generateReport() {
     let headTableElements = document.querySelectorAll('tr th input');
 
-    for (const element of headTableElements) {
-        
-            console.log(element.name)
-        
+    let indexes = [];
+
+    for (let i = 0; i < headTableElements.length; i++) {
+            if(headTableElements[i]['checked'] === true) {
+                indexes.push(i);
+            }
     }
+
+    let outputArr = [];
+
+    let rowsTableElements = document.querySelectorAll('tbody tr');
+
+    for (const row of rowsTableElements) {
+        
+        let tdElements = row.querySelectorAll('td');
+        let obj = {};
+        for(let i = 0; i < tdElements.length; i++) {
+            if(indexes.includes(i)){
+                obj[headTableElements[i].name] = tdElements[i].textContent;
+            }
+        }
+
+        outputArr.push(obj);
+    }
+
+    let outputJSONarr = outputArr.map(obj => JSON.stringify(obj, null, ' '));
+
+    let outputTextAreaElement = document.getElementById('output');
+
+    outputTextAreaElement.textContent = '[\n' + outputJSONarr.join(',\n') + '\n]';
 }
