dc1e194658d7f9f865914d7c8630b5c46f0cbc7e
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-03-19 12:26:49 +0200

    init

--- DOM-Events/DOM-Events-Lab/03. Add-Delete/app.js
+++ DOM-Events/DOM-Events-Lab/03. Add-Delete/app.js
@@ -1,3 +1,29 @@
 function addItem() {
-    //TODO...
+    let inputElement = document.getElementById('newItemText');
+
+    let textFromUser = inputElement.value;
+
+    let ulElement = document.getElementById('items');
+
+    let newElement = document.createElement('li');
+
+    newElement.textContent = textFromUser;
+
+    let deleteLinkElement = document.createElement('a');
+
+    deleteLinkElement.href = '#';
+
+    deleteLinkElement.textContent = '[Delete]';
+
+    deleteLinkElement.addEventListener('click', () => {
+
+        deleteLinkElement.parentElement.remove();
+    });
+
+
+    newElement.appendChild(deleteLinkElement);
+
+    ulElement.appendChild(newElement);
+
+    inputElement.value = '';
 }
--- DOM-Events/DOM-Events-Lab/04. Mouse-Gradient/app.js
+++ DOM-Events/DOM-Events-Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,22 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let divGradientElement = document.getElementById('gradient');
+
+    let resultDivElement = document.getElementById('result');
+
+    const gradientMousemoveHandler = (e) => {
+        const maxWidth = e.target.clientWidth - 1;
+
+        let currentWidth = e.offsetX;
+
+        let percent = Math.trunc((currentWidth / maxWidth) * 100);
+
+        resultDivElement.textContent = percent + '%';
+    }
+
+    const gradientMouseoutHandler = () => {
+        resultDivElement.textContent = '';
+    }
+
+    divGradientElement.addEventListener('mousemove', gradientMousemoveHandler)
+    divGradientElement.addEventListener('mouseout', gradientMouseoutHandler)
 }
