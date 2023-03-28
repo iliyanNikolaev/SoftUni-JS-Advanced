61185b3eef18a7f820c94dffbd3a2ff0e204e0c1
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-03-28 09:34:50 +0300

    update app.js
    This is the last version of code. Set event listeners on dynamically created buttons with event delegation.

--- Advanced-Functions/Advanced-Functions-Exr/10. Task Manager/app.js
+++ Advanced-Functions/Advanced-Functions-Exr/10. Task Manager/app.js
@@ -6,83 +6,66 @@ function solve() {
         date: document.getElementById('date')
     }
 
-    let [_, openArea, inProgressArea, finishArea] = document.querySelectorAll('section');
+    let [_, openArea, inProgressArea, finishArea] = document.querySelectorAll('section')
+    document.getElementById('add').addEventListener('click', functionality);
+
+    function functionality(ev) {
+    ev.preventDefault();
+
+    let task = inputs.task.value;
+    let description = inputs.description.value;
+    let date = inputs.date.value;
+
+    if (task !== '' && description !== '' && date !== '') {
+      let article = document.createElement('article');
+      article.className = 'delegationContainer'//this is the main container
+      let h3 = createElement('h3', task);
+      let pDesc = createElement('p', `Description: ${description}`);
+      let pDate = createElement('p', `Due Date: ${date}`);
+      let div = createElement('div', '', 'flex'); // this is the container for buttons
+      let startBtn = createElement('button', 'Start', 'green');
+      let deleteBtn = createElement('button', 'Delete', 'red');
+
+      article.appendChild(h3);
+      article.appendChild(pDesc);
+      article.appendChild(pDate);
+      div.appendChild(startBtn);
+      div.appendChild(deleteBtn);
+      article.appendChild(div);
+      openArea.appendChild(article);
+
+      inputs.task.value = '';
+      inputs.description.value = '';
+      inputs.date.value = '';
 
 
-    let addButton = document.getElementById('add');
+      article.addEventListener('click', (e) => {
+        if (e.target.className === 'red') {
+          article.remove();
+        } else if (e.target.className === 'green') {
+          e.target.remove();
 
-    addButton.addEventListener('click', add);
+          let finishBtn = document.createElement('button');
+          finishBtn.className = 'orange';
+          finishBtn.textContent = 'Finish';
+          div.appendChild(finishBtn);
+          inProgressArea.appendChild(article);
+        } else if (e.target.className === 'orange') {
+          e.target.remove();
+          deleteBtn.remove();
 
-    function add(event) {
-
-        event.preventDefault();
-
-        let task = inputs.task.value;
-        inputs.task.value = '';
-        let description = inputs.description.value;
-        inputs.description.value = '';
-        let date = inputs.date.value;
-        inputs.date.value = '';
-
-
-        if (task !== '' && description !== '' && date !== '') {
-
-            let article = document.createElement('article');
-
-            let h3 = createElement('h3', task);
-
-            let pDesc = createElement('p', `Description: ${description}`);
-
-            let pDate = createElement('p', `Due Date: ${date}`);
-
-            let div = createElement('div', '', 'flex');
-
-            let startBtn = createElement('button', 'Start', 'green');
-
-            let deleteBtn = createElement('button', 'Delete', 'red');
-
-            article.appendChild(h3);
-            article.appendChild(pDesc);
-            article.appendChild(pDate);
-            div.appendChild(startBtn);
-            div.appendChild(deleteBtn);
-            article.appendChild(div);
-
-            openArea.appendChild(article);
-
-            startBtn.addEventListener('click', () => {
-                startBtn.remove();
-
-                let finishBtn = createElement('button', 'Finish', 'orange');
-
-                div.appendChild(finishBtn);
-
-                inProgressArea.appendChild(article);
-
-                finishBtn.addEventListener('click', () => {
-                    finishBtn.remove();
-                    deleteBtn.remove();
-                    finishArea.appendChild(article);
-                });
-            });
-
-            deleteBtn.addEventListener('click', () => {
-                article.remove();
-            });
-
+          finishArea.appendChild(article);
         }
+      });
     }
+  }
 
     function createElement(type, text, clName) {
-
         let element = document.createElement(type);
-
         if (clName) {
             element.className = clName;
         }
-
         element.textContent = text;
-
         return element;
     }
 }
