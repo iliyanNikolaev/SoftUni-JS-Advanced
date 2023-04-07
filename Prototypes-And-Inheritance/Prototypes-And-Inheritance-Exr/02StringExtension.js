727ab49589caf5232d07d989ffe6b64302603149
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-04-07 19:30:56 +0300

    init

---
+++ Prototypes-And-Inheritance/Prototypes-And-Inheritance-Exr/02StringExtension.js
@@ -0,0 +1,55 @@
+(function(){ 
+    String.prototype.ensureStart = function(str){
+        if(!this.startsWith(str)){
+            return str + this;
+        } else {
+            return this.toString();
+        }
+    }
+    String.prototype.ensureEnd = function(str){
+        if(!this.endsWith(str)){
+            return this + str;
+        } else {
+            return this.toString();
+        }
+    }
+    String.prototype.isEmpty = function() {
+        return this === '' ? true : false;
+    }
+    String.prototype.truncate = function(n) {
+        if(n < 3) {
+            return '.'.repeat(n);
+        }
+        if(this.toString().length <= n){
+            return this.toString();
+        } else {
+            let lastIndex = this.toString().substring(0, n - 2).lastIndexOf(' ');
+
+            if(lastIndex !== -1){
+                return this.toString().substring(0, lastIndex) + '...';
+            } else {
+                return this.toString().substring(0, n - 3) + '...';
+            }
+        }
+    }
+
+    String.format = function (str, ...params){
+        for(let i = 0; i < params.length; i++){
+            str = str.replace(`{${i}}`, params[i]);
+        }
+        return str;
+    }
+})();
+
+let str = 'my string'
+console.log(str = str.ensureStart('my'))
+console.log(str = str.ensureStart('hello '))
+console.log(str = str.truncate(16))
+console.log(str = str.truncate(14))
+console.log(str = str.truncate(8))
+console.log(str = str.truncate(4))
+console.log(str = str.truncate(2))
+console.log(str = String.format('The {0} {1} fox',
+  'quick', 'brown'))
+console.log(str = String.format('jumps {0} {1}',
+  'dog'))
