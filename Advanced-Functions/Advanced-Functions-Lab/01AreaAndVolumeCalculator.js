fe1d55fa8c7314e2f75b71e86b7e18e90963c755
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-09-05 20:45:14 +0300

    some old examples from js advanced

--- Advanced-Functions/Advanced-Functions-Lab/01AreaAndVolumeCalculator.js
+++ Advanced-Functions/Advanced-Functions-Lab/01AreaAndVolumeCalculator.js
@@ -1,39 +1,70 @@
-function area() {
-    return Math.abs(this.x * this.y);
-};
+// function area() {
+//     return Math.abs(this.x * this.y);
+// };
 
-function vol() {
-    return Math.abs(this.x * this.y * this.z);
-};
+// function vol() {
+//     return Math.abs(this.x * this.y * this.z);
+// };
 
-function solve(area, vol, input) {
+// function solve(area, vol, input) {
     
-    let inputArr = JSON.parse(input);
+//     let inputArr = JSON.parse(input);
     
-    let output = []
-    inputArr.forEach(el => {
+//     let output = []
+//     inputArr.forEach(el => {
         
-        output.push({
-            area: area.call(el),
-            volume: vol.call(el)
-        })
-    });
+//         output.push({
+//             area: area.call(el),
+//             volume: vol.call(el)
+//         })
+//     });
 
-    console.log(output);
-}
+//     console.log(output);
+// }
 
-function areaVol(areaFn, volFn, input){
+// function areaVol(areaFn, volFn, input){
 
-    let inputArr = JSON.parse(input);
+//     let inputArr = JSON.parse(input);
 
-    inputArr.forEach(obj => {
-        console.log(`${areaFn.apply(obj)} <=> ${volFn.apply(obj)}`);
-    });
-}
+//     inputArr.forEach(obj => {
+//         console.log(`${areaFn.apply(obj)} <=> ${volFn.apply(obj)}`);
+//     });
+// }
 
-areaVol(area, vol, `[
-    {"x":"1","y":"2","z":"10"},
-    {"x":"7","y":"7","z":"10"},
-    {"x":"5","y":"2","z":"10"}
-    ]`
-    );
+// areaVol(area, vol, `[
+//     {"x":"1","y":"2","z":"10"},
+//     {"x":"7","y":"7","z":"10"},
+//     {"x":"5","y":"2","z":"10"}
+//     ]`
+//     );
+
+
+    //remastered
+
+    function area() {
+        return Math.abs(this.x * this.y);
+    };
+    
+    function vol() {
+        return Math.abs(this.x * this.y * this.z);
+    };
+
+    
+    function newSolve(area, vol, input) {
+        const arrOfObjects = JSON.parse(input);
+        const newArr = [];
+
+        arrOfObjects.forEach(element => {
+            newArr.push({
+                area: area.call(element),
+                volume: vol.call(element)
+            })
+        });
+    console.log(newArr);
+    }
+
+    newSolve(area, vol, `[
+        {"x":"1","y":"2","z":"10"},
+        {"x":"7","y":"7","z":"10"},
+        {"x":"5","y":"2","z":"10"}
+        ]`)
--- Advanced-Functions/Advanced-Functions-Lab/demo.js
+++ Advanced-Functions/Advanced-Functions-Lab/demo.js
@@ -1,9 +1,39 @@
-function add(num1) {
-    return function (num2) {
-        return function (num3) {
-            console.log(num1 + num2 + num3);
+// function add(num1) {
+//     return function (num2) {
+//         return function (num3) {
+//             console.log(num1 + num2 + num3);
+//         }
+//     }
+// }
+
+function add(num) {
+    return function(num2){
+        return function(num3) {
+            console.log(num + num2 + num3);
         }
     }
 }
 
-add(1)(2)(3); // curryng
+//add(1)(2)(3); // curryng
+ 
+// closure
+
+function createCounter() {
+    let counter = 0;
+
+    function increment() {
+        counter++;
+
+        console.log(counter);
+    }
+
+    return increment;
+}
+
+const counter = createCounter();
+
+counter();
+counter();
+counter();
+counter();
+counter();
