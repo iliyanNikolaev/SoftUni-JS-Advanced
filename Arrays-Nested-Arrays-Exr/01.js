82434c511e167b4afdc21e62894b7e4c44775e2d
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-09-19 11:26:25 +0300

    2 solutions from arrays exercise, js-advanced

---
+++ Arrays-Nested-Arrays-Exr/01.js
@@ -0,0 +1,9 @@
+function solve(input){
+    const output = [];
+
+    input.forEach((x, i) => i % 2 == 0 ? output.push(x) : null);
+
+    console.log(output.join(' '));
+}
+
+solve(['20', '30', '40', '50', '60'])
---
+++ Arrays-Nested-Arrays-Exr/02.js
@@ -0,0 +1,17 @@
+function solve(n, k) {
+    const output = [1];
+
+    for (let i = 1; i < n; i++) {
+        let element = 0;
+
+        i < k
+            ? output.slice(0, i).forEach(x => element += x)
+            : output.slice(-k).forEach(x => element += x)
+
+        output.push(element);
+    }
+
+    return output;
+}
+
+solve(8, 2)
